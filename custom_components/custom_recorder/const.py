8de5e97d57e75ad2c1d8db27e0c96586f3a18f6b
oukene <ragcarib@naver.com>
2023-09-06 17:14:15 +0900

    버전표기

--- custom_components/custom_recorder/const.py
+++ custom_components/custom_recorder/const.py
@@ -4,7 +4,7 @@ import numpy as np
 # name for the integration.
 DOMAIN = "custom_recorder"
 NAME = "Custom Recorder"
-VERSION = "1.1.0"
+VERSION = "1.2.0"
 
 CONF_DEVICE_NAME = "device_name"
 CONF_SOURCE_ENTITY = "source_entity"
